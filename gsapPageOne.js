307e6365f70274d66a855e272074b451cad772bd
naren67 <75604512+naren67@users.noreply.github.com>
2021-03-24 16:43:53 +0530

    Add files via upload

---
+++ gsapPageOne.js
@@ -0,0 +1,9 @@
+gsap.from('.p-pic',{opacity:0, duration:4, y:-300, scale:10, delay:3})
+gsap.from('.my-name',{opacity:0, duration:5, x:100, scale:3, delay:1})
+gsap.from('.name-tag',{opacity:0, duration:6, y:-800,scale:8, delay:1.6})
+gsap.from('.github-btn',{opacity:0, duration:5, y:500, scale:5, delay:1.6})
+gsap.from('.nav-logo',{opacity:0, duration:5, x:-500, delay:1.6})
+gsap.from('.quotes',{opacity:0, duration:6, y:-1500, scale:10, delay:2})
+
+
+
